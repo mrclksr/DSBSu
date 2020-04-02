3b2857f3516e8e91a18c4d3416425dfbe91e6d3a
Grégory Reinbold <gregory@nosheep.fr>
2020-04-02 12:09:12 +0200

    Add french translations

---
+++ locale/dsbsu_fr.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="../src/mainwin.cpp" line="37"/>
+        <source>Please enter the password for user %1</source>
+        <translation>Entrez le mot de passe de l&apos;utilisateur %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="51"/>
+        <source>&amp;Ok</source>
+        <translation>&amp;Ok</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="52"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancel</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="59"/>
+        <source>Execute &apos;%1&apos; as user %2
+</source>
+        <translation>Exécuter &apos;%1&apos; en tant qu&apos;utilisateur %2
+</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="113"/>
+        <source>Wrong password</source>
+        <translation>Mot de passe invalide</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="74"/>
+        <location filename="../src/main.cpp" line="103"/>
+        <source>No such user %1</source>
+        <translation>Utilisateur %1 non trouvé</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="92"/>
+        <source>Failed to execute command &apos;%1&apos;</source>
+        <translation>Impossible d&apos;exécuter la commande &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="106"/>
+        <source>su timed out</source>
+        <translation>su temps écoulé</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="109"/>
+        <source>Failed to execute su</source>
+        <translation>Impossible d&apos;exécuter su</translation>
+    </message>
+</context>
+</TS>
