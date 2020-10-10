bf93773f4ec9e2d2569fe5c617131c1095ee6999
Marcel <mk@freeshell.de>
2020-10-10 18:09:45 +0200

    Update

--- locale/dsbsu_fr.ts
+++ locale/dsbsu_fr.ts
@@ -4,29 +4,38 @@
 <context>
     <name>MainWin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="37"/>
+        <location filename="../src/mainwin.cpp" line="38"/>
+        <location filename="../src/mainwin.cpp" line="103"/>
         <source>Please enter the password for user %1</source>
         <translation>Entrez le mot de passe de l&apos;utilisateur %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="51"/>
+        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="117"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="53"/>
+        <location filename="../src/mainwin.cpp" line="118"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Cancel</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="59"/>
+        <location filename="../src/mainwin.cpp" line="60"/>
         <source>Execute &apos;%1&apos; as user %2
 </source>
         <translation>Exécuter &apos;%1&apos; en tant qu&apos;utilisateur %2
 </translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="113"/>
+        <location filename="../src/mainwin.cpp" line="124"/>
+        <source>&lt;qt&gt;The command &quot;&lt;tt&gt;%1&lt;/tt&gt;&quot; requires authentication
+&lt;/qt&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="184"/>
         <source>Wrong password</source>
         <translation>Mot de passe invalide</translation>
     </message>
@@ -34,23 +43,23 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/main.cpp" line="74"/>
-        <location filename="../src/main.cpp" line="103"/>
+        <location filename="../src/main.cpp" line="90"/>
+        <location filename="../src/main.cpp" line="119"/>
         <source>No such user %1</source>
         <translation>Utilisateur %1 non trouvé</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="92"/>
+        <location filename="../src/main.cpp" line="108"/>
         <source>Failed to execute command &apos;%1&apos;</source>
         <translation>Impossible d&apos;exécuter la commande &apos;%1&apos;</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="106"/>
+        <location filename="../src/main.cpp" line="122"/>
         <source>su timed out</source>
         <translation>su temps écoulé</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="109"/>
+        <location filename="../src/main.cpp" line="125"/>
         <source>Failed to execute su</source>
         <translation>Impossible d&apos;exécuter su</translation>
     </message>
