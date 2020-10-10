65fd8aaec7ff78c92176974957e465a4cde7ba32
Marcel <mk@freeshell.de>
2020-10-10 18:09:33 +0200

    Update

--- locale/dsbsu_de.ts
+++ locale/dsbsu_de.ts
@@ -4,29 +4,39 @@
 <context>
     <name>MainWin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="37"/>
+        <location filename="../src/mainwin.cpp" line="38"/>
+        <location filename="../src/mainwin.cpp" line="103"/>
         <source>Please enter the password for user %1</source>
         <translation>Bitte geben Sie das Passwort von %1 ein</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="51"/>
+        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="117"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="53"/>
+        <location filename="../src/mainwin.cpp" line="118"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="59"/>
+        <location filename="../src/mainwin.cpp" line="60"/>
         <source>Execute &apos;%1&apos; as user %2
 </source>
         <translation>Führe &apos;%1&apos; als Benutzer %2 aus
 </translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="113"/>
+        <location filename="../src/mainwin.cpp" line="124"/>
+        <source>&lt;qt&gt;The command &quot;&lt;tt&gt;%1&lt;/tt&gt;&quot; requires authentication
+&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;Der Befehl &quot;&lt;tt&gt;%1&lt;/tt&gt;&quot; erfordert Au­then­ti­fi­ka­ti­on
+&lt;/qt&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="184"/>
         <source>Wrong password</source>
         <translation>Falsches Passwort</translation>
     </message>
@@ -34,23 +44,23 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/main.cpp" line="74"/>
-        <location filename="../src/main.cpp" line="103"/>
+        <location filename="../src/main.cpp" line="90"/>
+        <location filename="../src/main.cpp" line="119"/>
         <source>No such user %1</source>
         <translation>Unbekannter Benutzer &apos;%1&apos;</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="92"/>
+        <location filename="../src/main.cpp" line="108"/>
         <source>Failed to execute command &apos;%1&apos;</source>
         <translation>Konnte Befehl &apos;%1&apos; nicht ausführen</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="106"/>
+        <location filename="../src/main.cpp" line="122"/>
         <source>su timed out</source>
         <translation>Keine Antwort von &apos;su&apos;</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="109"/>
+        <location filename="../src/main.cpp" line="125"/>
         <source>Failed to execute su</source>
         <translation>Konnte &apos;su&apos; nicht ausführen</translation>
     </message>
